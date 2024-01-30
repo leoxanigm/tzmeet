378355661dc67f3768967a2c846e89e6712ef49d
leoxanigm <zola@zola.dev>
2024-01-30 18:13:39 +0100

    update time display, remove current time to timezone conversion

--- public/js/userTimeZone.js
+++ public/js/userTimeZone.js
@@ -1,16 +1,9 @@
-//
-// To Do
-// - Change that users can only search and change time zone
-
-
 class Timezone {
-  #format = '24';
-  #timeNow = luxon.DateTime.now();
+  #format = '12';
+  #timeNow = null;
 
   constructor({ offsetData, zoneNameData }) {
-    this.offset = this.#timeNow.offset; // store current offset
-    this.zoneName = this.#timeNow.zoneName;
-
+    this.#updateTime();
     // imported json data (script tag)
     this.offsetData = offsetData;
     this.zoneNameData = zoneNameData;
@@ -25,25 +18,6 @@ class Timezone {
     return this.#timeNow.toFormat(format);
   }
 
-  /**
-   * Set current time specified by user
-   * @param {string} time of format 'HH:mm' or 'hh:mm a'
-   */
-  set time(time) {
-    // Detect if time is in 24h or 12h format
-    const format =
-      time.includes('AM') || time.includes('PM') ? 'hh:mm a' : 'HH:mm';
-
-    // Convert time to luxon DateTime object
-    this.#timeNow = luxon.DateTime.fromFormat(time, format);
-
-    this.#calculateOffset();
-  }
-
-  /**
-   * Set current time format
-   * @param {string} format - '24' or '12'
-   */
   set format(format) {
     format == '24' ? (this.#format = '24') : (this.#format = '12');
   }
@@ -52,37 +26,16 @@ class Timezone {
     return this.#format;
   }
 
-  #calculateOffset() {
-    // Special case for 45 minute offsets
-    const offset45 = [345, 525, 825, 765];
-
-    // calculate new offset based on current time
-    const milliseconds = this.#timeNow - luxon.DateTime.utc();
-    let offsetMinutes = milliseconds / 1000 / 60;
-
-    if (!offset45.includes(offsetMinutes)) {
-      offsetMinutes = Math.round(offsetMinutes / 30) * 30; // round to nearest multiple of 30
-
-      // Luxon only lets you set offset from current time zone. Hence, we need to
-      // add the current offset and then set the new time zone
-      offsetMinutes += this.offset;
-
-      // If offset is not in the data, find the nearest offset
-      while (this.offsetData[offsetMinutes] == undefined) {
-        offsetMinutes += 5;
-      }
-    }
-    this.offset = offsetMinutes;
+  get zone() {
+    return this.#timeNow.zoneName;
   }
 
-  #changeTimeZone() {
-    // change time zone
-    this.timeNow = this.timeNow.setZone(this.zoneName);
+  #updateTime() {
+    this.#timeNow = luxon.DateTime.now();
   }
 
-  #getZoneNameFromOffset() {
-    // get zone name from offset
-    this.zoneName = this.timezoneData[this.offset];
+  setZone(zoneName) {
+    this.#timeNow = luxon.Settings.defaultZone = zoneName;
   }
 
   searchLocation(searchTerm) {}
@@ -91,100 +44,60 @@ class Timezone {
 document.addEventListener('DOMContentLoaded', () => {
   const TZ = new Timezone(timezoneData); // timezoneData is loaded in a script tag
 
-  // Change display time and time zone on first load
-  updateTimeDisplay(TZ);
+  // Update time display every half second
+  setInterval(() => updateTimeDisplay(TZ), 500);
 
-  const timeModifyBtns = getNodes('.time-modify-btn');
-  timeModifyBtns.forEach((btn) =>
-    btn.addEventListener('click', (e) => modifyTime(e, TZ))
-  );
+  getNodes('input[name="time-format"]').forEach((radio) => {
+    radio.addEventListener('change', (e) => modifyFormat(e, TZ));
+  });
+
+  // const timeModifyBtns = getNodes('.time-modify-btn');
+  // timeModifyBtns.forEach((btn) =>
+  //   btn.addEventListener('click', (e) => modifyTime(e, TZ))
+  // );
 
   //   const timezoneConfig = new Timezone();
   //   console.log(timezoneConfig.timezoneData.offsetData['-660'][0])
 });
 
-function modifyTime(e, TZ) {
-  e.stopImmediatePropagation();
-
-  let value = e.currentTarget.dataset.value;
-  let type = e.currentTarget.dataset.type;
-
-  // Change time format
-  if (type === 'format') {
-    TZ.format = value;
-    updateTimeDisplay(TZ);
-    return;
-  }
-
-  if (type === 'hour' || type === 'minute' || type === 'meridiem') {
-    // Get current time
-    let newTimeNow = TZ.time;
-    const format = TZ.format;
-
-    // Change time based on type
-    if (type === 'hour') {
-      let hour = parseInt(newTimeNow.slice(0, 2));
-      hour += parseInt(value);
-      hour = ((hour + parseInt(format)) % parseInt(format)) + '';
-      hour = hour < 10 ? `0${hour}` : hour;
-      newTimeNow = newTimeNow.replace(/^[0-9]{2}/, hour);
-    } else if (type === 'minute') {
-      let minute = parseInt(newTimeNow.slice(3, 5));
-      minute += parseInt(value);
-      minute = ((minute + 60) % 60) + '';
-      minute = minute < 10 ? `0${minute}` : minute;
-      newTimeNow = newTimeNow.replace(/:[0-9]{2}/, `:${minute}`);
-    } else if (type === 'meridiem' && TZ.format === '12') {
-      newTimeNow = newTimeNow.replace(/ AM| PM/g, '');
-      newTimeNow += ` ${value}`;
-    }
-
-    TZ.time = newTimeNow;
-
-    // Finally change display time
-    // updateTimeDisplay(TZ);
-  }
-
-  // // Modify time based on type
-  // TZ.setTime(value, type);
-
-  // // Finally change display time
-  // updateTimeDisplay(timeInput.value);
-}
-
 function updateTimeDisplay(TZ) {
   // Get time display elements
-  const hourInput = getNodes('#hour-input');
-  const minuteInput = getNodes('#minute-input');
-  const amInput = getNodes('#time-am');
-  const pmInput = getNodes('#time-pm');
-  const format24Input = getNodes('#time-format-24');
-  const format12Input = getNodes('#time-format-12');
+  const hour = getNodes('.time-now .hour');
+  const minute = getNodes('.time-now .minute');
+  const meridiem = getNodes('.time-now .meridiem');
+  const secondTicker = getNodes('.time-now .second');
+  const formatToggle = getNodes('.time-format-toggle');
 
   let newTimeNow = TZ.time;
 
   // Change time display based on format
-
   if (TZ.format === '24') {
     // Hide am/pm toggle
-    getNodes('.am-pm-container').style.display = 'none';
+    meridiem.style.visibility = 'hidden';
 
     // Set time
-    [hourInput.value, minuteInput.value] = newTimeNow.split(':');
+    [hour.textContent, minute.textContent] = newTimeNow.split(':');
 
-    format24Input.checked = true;
+    formatToggle.querySelector('[value="24"]').checked = true;
   } else {
     // Show am/pm toggle
-    getNodes('.am-pm-container').style.display = 'inline-flex';
+    meridiem.style.visibility = 'visible';
 
     // Toggle am/pm
     const amOrPm = newTimeNow.slice(-2);
-    amInput.checked = true ? amOrPm == 'AM' : false;
-    pmInput.checked = true ? amOrPm == 'PM' : false;
+    newTimeNow = newTimeNow.slice(0, -3);
+    meridiem.textContent = amOrPm;
+    [hour.textContent, minute.textContent] = newTimeNow.split(':');
 
-    newTimeNow = newTimeNow.replace(/ AM| PM/g, '');
-    [hourInput.value, minuteInput.value] = newTimeNow.split(':');
-
-    format12Input.checked = true;
+    formatToggle.querySelector('[value="12"]').checked = true;
   }
+
+  secondTicker.classList.contains('hide')
+    ? secondTicker.classList.remove('hide')
+    : secondTicker.classList.add('hide');
+}
+
+function modifyFormat(e, TZ) {
+  TZ.format = e.target.value;
+  updateTimeDisplay(TZ);
 }
